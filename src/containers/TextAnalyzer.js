11692d998256f3e46e421f0d9af648a1700c781d
Daniel Epstein <d.epstein92@gmail.com>
2018-09-30 18:39:26 -0400

    added instructions modal to Text analyzer component

--- src/containers/TextAnalyzer.js
+++ src/containers/TextAnalyzer.js
@@ -2,39 +2,66 @@ import React, { Component } from 'react';
 import { StyleSheet, css } from 'aphrodite';
 import { Link } from 'react-router-dom';
 import { bindActionCreators, connect } from 'react-redux';
-import { Form, TextArea, Header, Segment } from 'semantic-ui-react';
+import { Form,
+         TextArea,
+         Header,
+         Segment,
+         Icon,
+         Modal,
+         Button } from 'semantic-ui-react';
 
 class TextAnalyzer extends React.Component{
 
  constructor(){
     super();
 
-    this.state = { void: '' }
+    this.state = { modalOpen: false }
     this.onSubmit = this.onSubmit.bind(this);
+    this.onOpenModal = this.onOpenModal.bind(this);
+    this.onCloseModal = this.onCloseModal.bind(this);
   }
 
  onSubmit(e){
    e.preventDefault();
  }
 
+ onCloseModal(){
+   this.setState({ modalOpen: false  });
+ }
+
+ onOpenModal(){
+   this.setState({ modalOpen: true });
+ }
+
  render(){
    return(
-     <div className={css(styles.container)}>
-     <Header attached='top' className={css(styles.title)}>
+  <div className={css(styles.container)}>
+    <Header attached='top' className={css(styles.title)}>
       Text Analyzer
     </Header>
-     <Segment className={css(styles.outerSegment)} attached>
-      <Form onSubmit={this.onSubmit}>
-       <TextArea className={css(styles.textArea)} />
-       <Form.Button>Submit</Form.Button>
-      </Form>
-     </Segment>
-     </div>
+    <div className={css(styles.modal)}>
+     <Modal trigger={<Button>Instructions</Button>} closeIcon>
+      <Header icon='question circle outline' content='Instructions' />
+      <Modal.Content>
+      <ul>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+      </ul>
+    </Modal.Content>
+   </Modal>
+   </div>
+   <Segment className={css(styles.outerSegment)} attached>
+    <Form onSubmit={this.onSubmit}>
+     <TextArea className={css(styles.textArea)} />
+     <Form.Button>Submit</Form.Button>
+     </Form>
+   </Segment>
+ </div>
    )
  }
 }
-
-
 
 
 const styles = StyleSheet.create({
@@ -62,6 +89,10 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     fontFamily: "Playfair Display, serif",
     fontSize: '3em'
+  },
+  modal: {
+    display: 'flex',
+    justifyContent: 'center',
   }
 })
 
