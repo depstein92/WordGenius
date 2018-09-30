cc34c83a39bd002d52d4e8f507fc6cc2e66e658b
Daniel Epstein <d.epstein92@gmail.com>
2018-09-30 15:53:44 -0400

    added try WordGenius button to About Component

--- src/components/About.js
+++ src/components/About.js
@@ -1,2 +1,75 @@
 import React from 'react';
 import { StyleSheet, css } from 'aphrodite';
+import { Segment, Button } from 'semantic-ui-react';
+
+
+const About = () => {
+  return(
+    <div className={css(style.aboutContainer)}>
+      <span className={css(style.aboutTitle)}>About: </span>
+      <Segment raised className={css(style.segment)}>
+       <br />
+       <br />
+       <p className={css(style.paragraph)}>
+       WordGenius is an text analyzer app that measures a conversations tone.
+       </p>
+       <br />
+       <br />
+       <p className={css(style.paragraph)}>
+       Based on IBM Watson keyword processing, it measures
+       </p>
+       <br />
+       <br />
+       <p className={css(style.paragraph)}>
+       the emotional tones of a conversation.
+       </p>
+       <br />
+       <br />
+       <br />
+       <br />
+       <br />
+       <br />
+       <br />
+       <br />
+       <br />
+       <Button content='Try WordGenius' icon='right arrow' labelPosition='right' />
+      </Segment>
+    </div>
+  )
+}
+
+const font = {
+  fontFamily: "Playfair Display, serif",
+  src: "https://fonts.googleapis.com/css?family=Playfair+Display"
+}
+
+const style = StyleSheet.create({
+  aboutContainer: {
+    height: '100vh',
+    width: '100vw',
+    top: '200vh',
+    position: 'absolute',
+    borderStyle: 'solid',
+    display: 'flex',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    borderTop: 'black'
+  },
+  aboutTitle:{
+    fontFamily: font.fontFamily,
+    fontSize: '5em',
+    position: 'relative'
+
+  },
+  segment: {
+    height: '60vh',
+    width: '60vw'
+  },
+  paragraph: {
+    fontSize: '2em',
+    fontFamily: font.fontFamily
+
+  }
+})
+
+export default About;
--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import HeroImage from './HeroImage';
 import Reviews from './Reviews';
+import About from './About';
 import { StyleSheet, css } from 'aphrodite';
 
 export default class App extends Component {
@@ -9,6 +10,7 @@ export default class App extends Component {
       <div className={css(styles.bodyStyle)}>
        <HeroImage />
        <Reviews />
+       <About />
       </div>
     );
   }
